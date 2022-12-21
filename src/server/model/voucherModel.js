7586cfa8e12f62a0581221bd888b08421209046a
ThanhTai2005 <btmthanhtai@gmail.com>
2022-12-21 23:30:37 +0700

    Change commentController and commentModel

---
+++ src/server/model/voucherModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const voucherSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    }, 
+    discount:{
+        type: Number,
+        required: true,
+    },
+    outDated:{
+        type: Date,
+        required: true,
+    },
+    maximum:{
+        type: Number,
+        required: true,
+    },
+    quatity:{
+        type: Number,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model('vouchers', voucherSchema);
